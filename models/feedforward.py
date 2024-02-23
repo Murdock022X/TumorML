3900ec0a3447d8023e3f9ef07ddac850e189b39a
Hank Murdock <hankmurdock@gmail.com>
2024-02-23 14:39:18 -0500

    Added Vgg19 Model

--- models/feedforward.py
+++ models/feedforward.py
@@ -1,21 +1,21 @@
 import tensorflow as tf
+from data.data import get_dataset
 
-class FeedForward:
+def make_model():
+    model = tf.keras.Sequential(
+        [
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(256, input_shape=(196608,), activation='relu'),
+            tf.keras.layers.Dense(128, activation='relu'),
+            tf.keras.layers.Dense(4, activation="softmax")
+        ]
+    )
 
-    def __init__(self):
-        self.model = tf.keras.Sequential(
-            [
-                tf.keras.layers.Flatten(),
-                tf.keras.layers.Dense(256, input_shape=(196608,), activation='relu'),
-                tf.keras.layers.Dense(128, activation='relu'),
-                tf.keras.layers.Dense(4, activation="softmax")
-            ]
-        )
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-        self.model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    return model
 
-    def fit(self, dataset):
-        self.model.fit(dataset, epochs=12)
-
-    def test(self, dataset):
-        self.model.evaluate(dataset)
+def train_test(model, epochs=10):
+    train_ds, test_ds = get_dataset()
+    model.fit(train_ds, epochs=epochs)
+    model.evaluate(test_ds)
---
+++ models/vgg.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+from data.data import get_dataset
+
+def make_model():
+    model = tf.keras.applications.vgg19.VGG19(
+        weights=None,
+        input_shape=(512,512,3),
+        classes=4
+    )
+
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    return model
+
+def train_test(model, epochs=10):
+    train_ds, test_ds = get_dataset()
+    model.fit(train_ds, epochs=epochs)
+    model.evaluate(test_ds)
