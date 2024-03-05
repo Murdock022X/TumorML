950c784eeddc5b08c2c87cb96ec992f9f629ed31
wright808 <mattyd1126@gmail.com>
2024-03-05 10:44:41 -0500

    Trained FFNN and VGG

--- train.py
+++ train.py
@@ -1,9 +1,7 @@
-# from models.ffnn import BrainFFNN
-# from models.vgg import BrainVGG
+from models.ffnn import BrainFFNN
+from models.vgg import BrainVGG
 
-# ffnn = BrainFFNN()
-# hist = ffnn.train(epochs=12)
-# ffnn.eval()
-# ffnn.save()
-import tensorflow as tf
-print(tf.config.list_physical_devices('GPU'))
+ffnn = BrainFFNN()
+hist = ffnn.train(epochs=12)
+ffnn.eval()
+ffnn.save()
